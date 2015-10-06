bf96b67f57d9038c661f39ca8e716a944a78d88f
t3kt <tommy.etkin@gmail.com>
2015-10-05 20:34:35 -0700

    reimplemented the par float slider component
    fixes problems with value scaling (closes #25).
    still need to clean out the old implementation

--- vjz/float_param_ext.py
+++ vjz/float_param_ext.py
@@ -1,11 +1,20 @@
 from vjz_params import VjzParam
 import vjz_util as util
 
+setattrs = util.setattrs
+overrideRows = util.overrideRows
+
 class FloatParam(VjzParam):
 	def __init__(self, comp):
 		VjzParam.__init__(self, comp)
 
 	def Initialize(self):
 		VjzParam.Initialize(self)
+		page = self.GetParamPage()
+		setattrs(page.appendMenu('Parsliderstyle', label='Slider Style')[0],
+		         menuNames=['std', 'xfade'],
+		         menuLabels=['Standard', 'X-Fade'])
 		slider = self._comp.op('slider')
 		self.ApplyBaseProxyExprs(slider)
+		util.ApplyPythonProxyExprs(slider, 'ext.vjzpar.par.',
+		                          Pctlsliderstyle='Parsliderstyle')
