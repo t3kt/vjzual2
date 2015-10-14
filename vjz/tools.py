bc57fbc9ede77be458b642d92577faf690b456f4
t3kt <tommy.etkin@gmail.com>
2015-10-13 20:59:13 -0700

    created tool for setting edit mode expressions
    see #62, #63

--- vjz/tools.py
+++ vjz/tools.py
@@ -1,6 +1,24 @@
 setattrs = mod.vjz_util.setattrs
+setexpr = mod.vjz_util.setexpr
+
+def getSelected():
+	pane = ui.panes.current
+	if pane.type == PaneType.NETWORKEDITOR:
+		selected= pane.owner.selectedChildren
+	else:
+		selected = []
+	print('tools.getSelected()', selected, 'pane', pane, 'type', pane.type, 'owner', pane.owner)
+	return selected
 
 def deletePars(o, *parNames):
 	pars = o.pars(*parNames)
 	for p in pars:
 		p.destroy()
+
+def setModParamEditModeExprs(params):
+	for p in params:
+		if not hasattr(p.par, 'Paruimode'):
+			print('setModParamEditModeExprs skipping', p)
+			continue
+		setexpr(p.par.Paruimode, 'ext.vjzmod.par.Modparuimode')
+		
