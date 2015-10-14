29f8465df73919a0c4d55d49c40b8ee453159cc7
t3kt <tommy.etkin@gmail.com>
2015-10-13 21:55:08 -0700

    added tools for setting align order based on nodeX/nodeY
    closes #70.
    part of #63

--- vjz/tools.py
+++ vjz/tools.py
@@ -1,3 +1,5 @@
+from numpy import interp
+
 setattrs = mod.vjz_util.setattrs
 setexpr = mod.vjz_util.setexpr
 
@@ -15,10 +17,43 @@ def deletePars(o, *parNames):
 	for p in pars:
 		p.destroy()
 
-def setModParamEditModeExprs(params):
+def setModParamEditModeExprs():
+	params = getSelected()
 	for p in params:
 		if not hasattr(p.par, 'Paruimode'):
 			print('setModParamEditModeExprs skipping', p)
 			continue
 		setexpr(p.par.Paruimode, 'ext.vjzmod.par.Modparuimode')
-		
+
+# def _createRangeValGetter(startVal, endVal):
+# 	return lambda i, n: interp(float(i), [0.0, float(n)-1.0], [startVal, endVal])
+#
+# def _setAttrsInOrder(objs, sortAttr, setAttr, calcVal=None, startVal=0.0, endVal=1.0):
+# 	if calcVal is None:
+# 		calcVal = _createRangeValGetter(startVal, endVal)
+# 	if isinstance(sortAttr, str):
+# 		sortAttrName = sortAttr
+# 		sortAttr = lambda o: getattr(o, sortAttrName)
+# 	sortedObjs = sorted(objs, key=sortAttr)
+# 	if isinstance(setAttr, str):
+# 		setAttrName = setAttr
+# 		setAttr = lambda o, v: setattr(o, setAttrName, v)
+# 	n = len(sortedObjs)
+# 	for i in range(n):
+# 		obj = sortedObjs[i]
+# 		val = calcVal(i, n)
+# 		setAttr(obj, val)
+
+def setAlignOrderBy(sortAttrName):
+	selected = getSelected()
+	n = len(selected)
+	selected = sorted(selected, key=lambda o: getattr(o, sortAttrName))
+	for i in range(n):
+		val = interp(float(i), [0, n-1], [0.0, 1.0])
+		selected[i].par.order = val
+
+def setAlignOrderByX():
+	setAlignOrderBy('nodeX')
+
+def setAlignOrderByY():
+	setAlignOrderBy('nodeY')
