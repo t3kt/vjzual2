ea631573a3c12689b125ef8603bbf68387cdbd5b
t3kt <tommy.etkin@gmail.com>
2015-10-17 21:39:53 -0700

    Revert "added normalized output for menu params"
    This reverts commit 13ed75c85b9c997aa11c262476cfc803c83b0343.

--- vjz/menu_param_ext.py
+++ vjz/menu_param_ext.py
@@ -7,7 +7,6 @@ class MenuParam(VjzParam):
 
 	def Initialize(self):
 		VjzParam.Initialize(self)
-		comp = self._comp
 		page = self.GetParamPage()
 		util.setattrs(page.appendInt('Parlistsize', label='List Items')[0],
 					  min=1,
@@ -15,22 +14,14 @@ class MenuParam(VjzParam):
 					  clampMin=1,
 					  normMax=10,
 					  default=5)
-		util.setattrs(page.appendInt('Parnumoptions', label='Number of Options')[0],
-		              min=1,
-		              clampMin=True,
-		              default=1)
-		menu = comp.op('menu')
+		menu = self._comp.op('menu')
 		self.ApplyBaseProxyExprs(menu)
 		util.ApplyPythonProxyExprs(menu, 'ext.vjzpar.par.',
 		                          Pctllistsize='Parlistsize')
-		util.setexpr(comp.par.Partype, '"menu"')
+		util.setexpr(self._comp.par.Partype, '"menu"')
 		util.setParExprs(menu,
 		                 w='op("rootpanel").par.w - (0 if parent().par.Parhidelabel else op("label").par.w)',
 		                 h='op("rootpanel").par.h')
-		par = self.TargetPar
-		if par is not None:
-			util.setParExprs(comp,
-			                 Parnumoptions='len(me.TargetPar.menuNames)')
 
 	@property
 	def TargetPar(self):
