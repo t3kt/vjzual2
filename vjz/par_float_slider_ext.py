17e1355b0a6db7e4f3261cd021f4ad079af297c1
t3kt <tommy.etkin@gmail.com>
2015-11-06 20:05:05 -0800

    use normVal sometimes and try to fix some of the slider init problems
    see #143

--- vjz/par_float_slider_ext.py
+++ vjz/par_float_slider_ext.py
@@ -1,31 +1,39 @@
-from vjz_params import ParControl
-import vjz_util as util
+if False:
+	from vjz.params import ParControl
+else:
+	from vjz_params import ParControl
+if False:
+	import vjz.util as util
+else:
+	import vjz_util as util
 from numpy import interp
 
 class ParFloatSlider(ParControl):
 	def __init__(self, comp):
 		ParControl.__init__(self, comp)
+		self.ParNormMin = tdu.Dependency(0)
+		self.ParNormMax = tdu.Dependency(1)
+		self.ParDefault = tdu.Dependency(0)
 
 	def Initialize(self):
 		ParControl.Initialize(self)
 		self._comp.par.top = './bg'
 		self._comp.par.chop = './inputval'
-		attrs = self._comp.op('set_par_attrs')
-		attrs.clear()
+		self.UpdateParSettings()
+
+	def UpdateParSettings(self):
 		p = self.TargetPar
 		if p is not None:
-			attrs.appendChan('normMin').vals=[p.normMin]
-			attrs.appendChan('normMax').vals=[p.normMax]
-			attrs.appendChan('default').vals=[p.default]
-		else:
-			attrs.appendChan('normMin').vals=[0]
-			attrs.appendChan('normMax').vals=[1]
-			attrs.appendChan('default').vals=[0]
+			self.ParNormMin.val = p.normMin
+			self.ParNormMax.val = p.normMax
+			self.ParDefault.val = p.default
 
 	def PullValue(self):
 		p = self.TargetPar
 		if p is not None:
-			attrs = self._comp.op('par_attrs')
-			raw = p.eval()
-			scaled = interp(raw, [attrs['normMin'][0], attrs['normMax'][0]], [0.0, 1.0])
-			self._comp.panel.u = scaled
+			if p.mode == ParMode.CONSTANT:
+				self._comp.panel.u = p.normVal
+			else:
+				raw = p.eval()
+				scaled = interp(raw, [p.normMin, p.normMax], [0.0, 1.0])
+				self._comp.panel.u = scaled
