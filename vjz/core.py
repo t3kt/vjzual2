bcd14f86691b5cb6d3bab5151ae08331d0845f14
t3kt <tommy.etkin@gmail.com>
2015-11-12 20:18:47 -0800

    added UI for load/save state
    closes #2

--- vjz/core.py
+++ vjz/core.py
@@ -7,9 +7,18 @@ else:
 
 import json
 
+def readState(fname):
+	with open(fname, 'r') as f:
+		return json.load(f)
+
+def writeState(fname, state):
+	with open(fname, 'w') as f:
+		json.dump(state, f, indent=2)
+
 class VjzSystem:
 	def __init__(self, root):
 		self.root = root
+		self.stateMeta = {}
 
 	def GetModules(self, hidden=False):
 		return self.root.findChildren(tags=['vjzmod'], key=lambda m: not m.par.Modhidden or hidden)
@@ -20,14 +29,14 @@ class VjzSystem:
 			print('building state for module: %s' % (m.path,))
 			modStates.append(m.GetStateDict())
 		return {
-			"modules": modStates
+			'modules': modStates,
+			'meta': self.root.fetch('stateMeta', {}),
 		}
 
 	def SaveState(self, fname):
 		state = self.GetState()
 		print('Saving state to %s: %r' % (fname, state))
-		with open(fname, 'w') as f:
-			json.dump(state, f, indent=2)
+		writeState(fname, state)
 
 	def SetState(self, state):
 		print('Loading state %r' % state)
@@ -36,9 +45,9 @@ class VjzSystem:
 				m = self.root.op(modState['path'])
 				if m:
 					m.LoadStateDict(modState)
+		self.root.store('stateMeta', state.get('meta', {}))
 
 	def LoadState(self, fname):
 		print('Loading state from %s' % (fname,))
-		with open(fname, 'r') as f:
-			state = json.load(f)
+		state = readState(fname)
 		self.SetState(state)
